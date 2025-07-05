7e5fd71bcc72127f4f6552a299e7ace13e04dba6
anevilvn <pnttoan1474@gmail.com>
2025-07-05 17:07:37 +0700

    Cập nhật: Cải tiến entrypoint.sh để sử dụng biến môi trường PORT

--- odoo-18-docker-compose/entrypoint.sh
+++ odoo-18-docker-compose/entrypoint.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-odoo -c /etc/odoo/odoo.conf -i base,asset_management,custom_auth,fund_management,investor_profile_management,overview_fund_management,transaction_management --without-demo=all
+# Render provides a PORT environment variable that we must use.
+# Default to 8069 if PORT is not set (for local execution).
+ODOO_HTTP_PORT=${PORT:-8069}
+
+# Start Odoo
+# --http-port: Use the port assigned by Render.
+# --http-interface 0.0.0.0: Listen on all network interfaces, crucial for Render.
+# --no-longpolling: Disable the longpolling port as we don't need it and it can cause port conflicts.
+odoo -c /etc/odoo/odoo.conf \
+     --http-port $ODOO_HTTP_PORT \
+     --http-interface 0.0.0.0 \
+     --no-longpolling \
+     -i fund_management,custom_auth,investor_profile_management,asset_management,overview_fund_management,transaction_management \
+     --db_host $DB_HOST \
+     --db_port $DB_PORT \
+     --db_user $DB_USER \
+     --db_password $DB_PASSWORD \
+     --database $DB_DATABASE \
+     --load-language=vi_VN
